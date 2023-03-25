f04b5617210913742e0d048a62dac0699a007431
Zachary Thurston <zwt88888@gmail.com>
2023-03-25 14:05:17 -0400

    l

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!bin/bash 
-cd ~/javamop/
+cd ../javamop/
 mvn clean
 bash scripts/install-javaparser.sh
 mvn package -Drat.skip -DskipTests
 
-cd ~/rv-violation-db/extension/
+cd ../rv-violation-db/extension/
 mvn package
 mvn_dir=$(mvn -version | grep ^Maven | cut -d: -f2 | tr -d ' ')
 mkdir -p ${mvn_dir}/lib/ext
 cp target/mop-extension-1.0-SNAPSHOT.jar ${mvn_dir}/lib/ext
 
-cd ~/javamop-agent-bundle/
+cd ../javamop-agent-bundle/
 bash make-agent.sh props/ agents/ quiet
 mvn install:install-file -Dfile=agents/JavaMOPAgent.jar -DgroupId="javamop-agent" -DartifactId="javamop-agent" -Dversion="1.0" -Dpackaging="jar"
 export RVMLOGGINGLEVEL=UNIQUE
