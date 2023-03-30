9765ffe87bbdef273fca749c968badc65a530759
Zachary William Thurston <zwt3@paz.cs.cornell.edu>
2023-03-30 16:29:22 -0400

    Removed script invocations from javamopEnv

--- vio.sh
+++ vio.sh
@@ -11,20 +11,20 @@ USAGE
     exit 1
 }
 
-# function install_extension() {
-#     cd ~/javamop/
-#     mvn clean
-#     bash scripts/install-javaparser.sh
-#     mvn package -Drat.skip -DskipTests
+function install_extension() {
+    cd ~/javamop/
+    mvn clean
+    bash scripts/install-javaparser.sh
+    mvn package -Drat.skip -DskipTests
 
-#     cd ~/rv-violation-db/extension/
-#     mvn package
-#     mvn_dir=$(mvn -version | grep ^Maven | cut -d: -f2 | tr -d ' ')
-#     mkdir -p ${mvn_dir}/lib/ext
-#     cp target/mop-extension-1.0-SNAPSHOT.jar ${mvn_dir}/lib/ext
+    cd ~/rv-violation-db/extension/
+    mvn package
+    mvn_dir=$(mvn -version | grep ^Maven | cut -d: -f2 | tr -d ' ')
+    mkdir -p ${mvn_dir}/lib/ext
+    cp target/mop-extension-1.0-SNAPSHOT.jar ${mvn_dir}/lib/ext
 
-#     cd ~/javamop-agent-bundle/
-# }
+    cd ~/javamop-agent-bundle/
+}
 
 function setup_prop() {
     ###HANDLE SPECIFIC AGENTS###
@@ -63,6 +63,7 @@ mkdir ~/violations-data/
 while [ "$1" != "" ]; do
     case $1 in
     --violation-id)
+        install_extension
         shift # remove `-t` or `--tag` from `$1`
         VIO_ID=$1
         echo $VIO_ID
