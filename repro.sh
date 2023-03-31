a65dd99fc8c1123fe0a3bb52f15e922b3e1dcfe7
Zachary William Thurston <zwt3@paz.cs.cornell.edu>
2023-03-31 16:09:27 -0400

    updated javamopEnv

--- repro.sh
+++ repro.sh
@@ -9,6 +9,6 @@ VIO_ID=$1
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-
+#bash make_dockerfile.sh ${VIO_ID}
 image=violation-${VIO_ID}:latest
 docker run -t --rm -v ${SCRIPT_DIR}:/Scratch ${image} /bin/bash -x /Scratch/run_entrypoint.sh ${VIO_ID} &> log.txt
