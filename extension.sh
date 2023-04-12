bdf71d1b4ea12c2400fa693c0456863c85c4a43d
Zachary William Thurston <zwt3@paz.cs.cornell.edu>
2023-04-12 12:35:02 -0400

    updated structure

--- extension.sh
+++ extension.sh
@@ -3,6 +3,7 @@
 cd ~/rv-violation-db/extension/
 mvn_dir=$(mvn -version | grep ^Maven | cut -d: -f2 | tr -d ' ')
 mkdir -p ${mvn_dir}/lib/ext
-cp target/mop-extension-1.0-SNAPSHOT.jar ${mvn_dir}/lib/ext
+cp target/mop-extension-1.0-SNAPSHOT.jar ${mvn_dir}/lib/ext &&
+echo "Succeeded moving extenion"
 
 cd ~/javamop-agent-bundle/
