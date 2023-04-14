97111c6002c9bfb72dc00b2f5cefbfb3c0eca654
Zachary William Thurston <zwt3@paz.cs.cornell.edu>
2023-04-14 11:50:11 -0400

    editing scripts

--- vio.sh
+++ vio.sh
@@ -6,21 +6,6 @@ USAGE
     exit 1
 }
 
-# function install_extension() {
-#     cd ~/javamop/
-#     mvn clean
-#     bash scripts/install-javaparser.sh
-#     mvn package -Drat.skip -DskipTests
-
-#     cd ~/rv-violation-db/extension/
-#     mvn package
-#     mvn_dir=$(mvn -version | grep ^Maven | cut -d: -f2 | tr -d ' ')
-#     mkdir -p ${mvn_dir}/lib/ext
-#     cp target/mop-extension-1.0-SNAPSHOT.jar ${mvn_dir}/lib/ext
-
-#     cd ~/javamop-agent-bundle/
-# }
-
 function setup_prop() {
     ###HANDLE SPECIFIC AGENTS###
     mkdir ~/javamop-agent-bundle/props-to-use/
