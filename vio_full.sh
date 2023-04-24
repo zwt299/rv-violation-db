6d670d3805acc1337bde99386b48fdb1446fab45
Zachary William Thurston <zwt3@paz.cs.cornell.edu>
2023-04-24 12:39:03 -0400

    Updated scripts

--- vio_full.sh
+++ vio_full.sh
@@ -27,7 +27,7 @@ function setup_repo_and_test() {
 # Then Clone the Project that you are trying to work on
     cd ~/javamop-agent-bundle/
     git clone https://github.com/$SLUG
-    echo $TEST_DIR
+    echo $TEST
     cd ~/javamop-agent-bundle/$TEST_DIR
     git checkout $SHA
     echo $TEST
